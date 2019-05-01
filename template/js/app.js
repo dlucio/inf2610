c89b03cf8280a56710b3f77707860dbd203b047b
Djalma Lucio <djalma.lucio@gmail.com>
2019-05-01 02:33:35 -0300

    Funcionando, mas vou limpar o código

--- template/js/app.js
+++ template/js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Important references
 // https://threejs.org/docs/index.html#api/en/renderers/webgl/WebGLProgram
 // https://threejs.org/docs/index.html#manual/en/introduction/How-to-use-WebGL2
@@ -17,6 +19,8 @@ function init() {
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color( 0x8FBCD4 );
+
+  loadImage();
 
   createCamera();
   createControls();
@@ -71,7 +75,7 @@ function createLights() {
 
 function createMaterial() {
 
-  const uniforms = {}
+  let uniforms = {}
   uniforms.ka = new THREE.Uniform(new THREE.Vector4(0.21, 0.13, 0.05, 1.0));
   uniforms.kd = new THREE.Uniform(new THREE.Vector4(0.71, 0.43, 0.18, 1.0));
   uniforms.ks = new THREE.Uniform(new THREE.Vector4(1.0, 1.0, 1.0, 1.0));
@@ -86,21 +90,24 @@ function createMaterial() {
   texture.encoding = THREE.sRGBEncoding;
   texture.anisotropy = 16;
 
-  uniforms.mytexture = { 
+  // uniforms.tex = new THREE.Uniform(new THREE.Texture(texture));
+  uniforms.tex = { 
     type: "t",
     value: texture 
-  }
+  } 
+
+  // Tip from: https://github.com/mrdoob/three.js/issues/8016#issuecomment-194935980
+  uniforms = THREE.UniformsUtils.merge([uniforms, THREE.UniformsLib['lights']]);
+  uniforms.tex.value = texture;
+  
 
   
   material = new THREE.ShaderMaterial({
-    uniforms: THREE.UniformsUtils.merge([ 
-      uniforms, 
-      THREE.UniformsLib['lights']
-    ]),
+    uniforms: uniforms,
     vertexShader: document.getElementById("vs").textContent.trim(),
     fragmentShader: document.getElementById("fs").textContent.trim(),
     lights: true,
-    defines: { USE_MAP: ''},
+    // defines: { USE_MAP: '', STANDARD: ''},
   });
 }
 
@@ -115,13 +122,10 @@ function createMeshes() {
   texture.encoding = THREE.sRGBEncoding;
   texture.anisotropy = 16;
 
-  // const material = new THREE.MeshStandardMaterial( {
-  //   map: texture,
-  // } );
-
   mesh = new THREE.Mesh( geometry, material );
 
   scene.add( mesh );
+  console.log("MESH", mesh);
 
 }
 
@@ -183,6 +187,47 @@ function onWindowResize() {
 }
 
 window.addEventListener( 'resize', onWindowResize );
+
+function loadImage() {
+
+  const geometry = new THREE.BoxBufferGeometry(.9,.9,.9);
+
+  // Create a texture loader so we can load our image file
+  const loader = new THREE.TextureLoader();
+
+  // specify the url to the texture
+  const url = 'https://s3.amazonaws.com/duhaime/blog/tsne-webgl/assets/cat.jpg';
+
+  const textureLoader = new THREE.TextureLoader();
+
+  const texture = textureLoader.load( url );
+
+  texture.encoding = THREE.sRGBEncoding;
+  texture.anisotropy = 16;
+
+  const uniforms = {};
+  uniforms.tex = { 
+    type: "t",
+    value: texture 
+  }
+
+  // specify custom uniforms and attributes for shaders
+  // Uniform types: https://github.com/mrdoob/three.js/wiki/Uniforms-types
+  const material = new THREE.ShaderMaterial({  
+    uniforms: uniforms,
+    vertexShader: document.getElementById('vertex-shader').textContent,
+    fragmentShader: document.getElementById('fragment-shader').textContent
+  });
+
+  // Combine our image geometry and material into a mesh
+  const mesh1 = new THREE.Mesh(geometry, material);
+
+  // Set the position of the image mesh in the x,y,z dimensions
+  mesh1.position.set(-3,0,0)
+
+  // Add the image to the scene
+  scene.add(mesh1);
+}
 
 // call the init function to set everything up
 init();
