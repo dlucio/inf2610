24148e4e3a70af737bc6836acc6e58a3f9776e2b
Djalma Lúcio <dlucio@impa.br>
2019-07-08 02:43:57 -0300

    Carregando todos os modelos de uma única vez

--- projeto/newproject/app-ssao.js
+++ projeto/newproject/app-ssao.js
@@ -34,10 +34,9 @@ import {
     OrbitControls
 } from './jsm/controls/OrbitControls.js';
 
-var container, stats;
-var camera, scene, renderer;
-var composer;
-var group;
+let container, stats;
+let camera, scene, renderer;
+let composer;
 let controls;
 
 init();
@@ -62,14 +61,6 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // Not used with webgl2 context
-    // if ( ! renderer.extensions.get( 'WEBGL_depth_texture' ) ) {
-
-    //   document.querySelector( '#error' ).style.display = 'block';
-    //   return;
-
-    // }
-
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(50, 70, 95.0);
 
@@ -84,39 +75,15 @@ function init() {
 
     loadModelAndMaterial();
 
-    // group = new THREE.Group();
-    // // scene.add( group );
-
-    // var geometry = new THREE.BoxBufferGeometry(10, 10, 10);
-
-    // for (var i = 0; i < 100; i++) {
-
-    //     var material = new THREE.MeshLambertMaterial({
-    //         color: Math.random() * 0xffffff
-    //     });
-
-    //     var mesh = new THREE.Mesh(geometry, material);
-    //     mesh.position.x = Math.random() * 400 - 200;
-    //     mesh.position.y = Math.random() * 400 - 200;
-    //     mesh.position.z = Math.random() * 400 - 200;
-    //     mesh.rotation.x = Math.random();
-    //     mesh.rotation.y = Math.random();
-    //     mesh.rotation.z = Math.random();
-
-    //     mesh.scale.setScalar(Math.random() * 10 + 2);
-    //     group.add(mesh);
-
-    // }
-
     stats = new Stats();
     container.appendChild(stats.dom);
 
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
 
     composer = new EffectComposer(renderer);
 
-    var ssaoPass = new SSAOPass(scene, camera, width, height);
+    let ssaoPass = new SSAOPass(scene, camera, width, height);
     ssaoPass.kernelRadius = 16;
     ssaoPass.ssaoMaterial.vertexShader = document.getElementById('ssao-vs').textContent.trim();
     ssaoPass.ssaoMaterial.fragmentShader = document.getElementById('ssao-fs').textContent.trim();
@@ -127,13 +94,13 @@ function init() {
     composer.addPass(ssaoPass);
 
     // Init gui
-    var gui = new GUI();
+    let gui = new GUI();
 
     gui.add(ssaoPass, 'output', {
         'Complete': SSAOPass.OUTPUT.Default,
         'SSAO Only': SSAOPass.OUTPUT.SSAO,
         'SSAO Only + Blur': SSAOPass.OUTPUT.Blur,
-        'Lighting': SSAOPass.OUTPUT.Beauty,
+        'Lighting Only': SSAOPass.OUTPUT.Beauty,
         'Depth': SSAOPass.OUTPUT.Depth,
         'Normal': SSAOPass.OUTPUT.Normal
     }).onChange(function (value) {
@@ -153,8 +120,8 @@ function init() {
 
 function onWindowResize() {
 
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
 
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
@@ -198,16 +165,18 @@ function loadModelAndMaterial() {
 
     // A reusable function to set up the models. We're passing in a position parameter
     // so that they can be individually placed around the scene
-    const onObjLoad = (obj, position, scale) => {
+    const onObjLoad = (obj, position, scale, visible=true) => {
 
         let model = obj.children;
         model = model[0];
         model.position.copy(position);
         model.scale.copy(scale);
 
+        model.visible = visible;
+
         // https://github.com/mrdoob/three.js/issues/12402
         // https://threejs.org/docs/#examples/utils/BufferGeometryUtils
-        BufferGeometryUtils.computeTangents(model.geometry);
+        // BufferGeometryUtils.computeTangents(model.geometry);
 
         scene.add(model);
 
@@ -221,30 +190,35 @@ function loadModelAndMaterial() {
     let s = 0.1;
     const ponyCartoonPosition = new THREE.Vector3(0, 0, 0);
     const ponyCartoonScale = new THREE.Vector3(0.1, 0.1, 0.1);
-    objLoader.load( 'models/pony_cartoon/Pony_cartoon.obj', obj => onObjLoad( obj, ponyCartoonPosition, ponyCartoonScale ), onProgress, onError );
+    objLoader.load( 'models/pony_cartoon/Pony_cartoon.obj', 
+        obj => onObjLoad( obj, ponyCartoonPosition, ponyCartoonScale, true ), onProgress, onError );
 
     // Modelo adicional para testes com o SSAO
     s = 0.45;
     const superHumanPosition = new THREE.Vector3(0, 0, 0);
     const superHumanScale = new THREE.Vector3(s, s, s);
-    // objLoader.load('models/super_human/super_human.obj', obj => onObjLoad(obj, superHumanPosition, superHumanScale), onProgress, onError);
+    objLoader.load('models/super_human/super_human.obj', 
+        obj => onObjLoad(obj, superHumanPosition, superHumanScale, false), onProgress, onError);
 
     // Modelo adicional para testes com o SSAO
     s = 10.00;
     const mechM6kPosition = new THREE.Vector3(0, 20, 0);
     const mechM6kScale = new THREE.Vector3(s, s, s);
-    // objLoader.load( 'models/mech-m-6k/mech-m-6k.obj', obj => onObjLoad( obj, mechM6kPosition, mechM6kScale ), onProgress, onError );
+    objLoader.load( 'models/mech-m-6k/mech-m-6k.obj', 
+        obj => onObjLoad( obj, mechM6kPosition, mechM6kScale, false ), onProgress, onError );
 
     // Modelo adicional para testes com o SSAO
     s = 8.00;
     const catterpillar789cPosition = new THREE.Vector3(0, 20, 0);
     const catterpillar789cScale = new THREE.Vector3(s, s, s);
-    // objLoader.load( 'models/catterpillar-789c/catterpillar-789c.obj', obj => onObjLoad( obj, catterpillar789cPosition, catterpillar789cScale ), onProgress, onError );
+    objLoader.load( 'models/catterpillar-789c/catterpillar-789c.obj', 
+        obj => onObjLoad( obj, catterpillar789cPosition, catterpillar789cScale, false ), onProgress, onError );
 
     // Modelo adicional para testes com o SSAO
     s = 6.00;
     const utilitarianVericlePosition = new THREE.Vector3(0, 20, 0);
     const utilitarianVericleScale = new THREE.Vector3(s, s, s);
-    // objLoader.load( 'models/utilitarian-vehicle/utilitarian-vehicle.obj', obj => onObjLoad( obj, utilitarianVericlePosition, utilitarianVericleScale ), onProgress, onError );
+    objLoader.load( 'models/utilitarian-vehicle/utilitarian-vehicle.obj', 
+        obj => onObjLoad( obj, utilitarianVericlePosition, utilitarianVericleScale, false ), onProgress, onError );
 
 }
